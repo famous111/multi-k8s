b34a8dc53b0816b8a5a5293b961b044798819cca
Ming Zhu <zhumphy@gmail.com>
2021-06-20 21:27:47 -0400

    correct typo

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@ docker build -t michaelzhu101/multi-worker:latest -t michaelzhu101/multi-worker:
 # Log in to the docker CLI
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 # Take those images and push them to docker hub
-docker push michaelzhu101/multi-client:lastest
-docker push michaelzhu101/multi-server:lastest
-docker push michaelzhu101/multi-worker:lastest
+docker push michaelzhu101/multi-client:latest
+docker push michaelzhu101/multi-server:latest
+docker push michaelzhu101/multi-worker:latest
 docker push michaelzhu101/multi-client:$SHA
 docker push michaelzhu101/multi-server:$SHA
 docker push michaelzhu101/multi-worker:$SHA
 #  apply all configs in the k8s folder
 kubectl apply -f k8s
-kubectl set image deployments/server-deployments server=michaelzhu101/multi-server:$SHA
-kubectl set image deployments/client-deployments server=michaelzhu101/multi-client:$SHA
-kubectl set image deployments/worker-deployments server=michaelzhu101/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=michaelzhu101/multi-server:$SHA
+kubectl set image deployments/client-deployment server=michaelzhu101/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=michaelzhu101/multi-worker:$SHA
